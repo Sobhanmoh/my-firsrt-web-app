6f4017dff63db5ee714c69d6d03a2210485c6a8d
sobhanmoh <kingsobhan57@gmail.com>
2024-08-27 23:26:05 -0700

    sobhan

--- html.js.css/my log in 1/index.js
+++ html.js.css/my log in 1/index.js
@@ -16,3 +16,17 @@ but2. addEventListener('click', function () {
 but3. addEventListener('click', function () {
     email.textContent = ''
 })
+
+
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+}
